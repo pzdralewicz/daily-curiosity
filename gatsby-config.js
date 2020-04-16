354c151b9bfc7ef19488494b7fc2ee5aba6c9c15
Paweł Zdralewicz <pawel.zdralewicz@gmail.com>
2020-04-17 00:48:24 +0200

    Add remove-serviceworker

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,8 @@ module.exports = {
           "/sw.js": ["cache-control: public, max-age=0, must-revalidate"],
         }
       }
-    }
+    },
+    'gatsby-plugin-remove-serviceworker'
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
