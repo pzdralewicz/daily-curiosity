2e9d7fdb29f6f7cc45ced6c60d4855ad3119d2dc
Paweł Zdralewicz <pawel.zdralewicz@gmail.com>
2020-05-20 19:40:34 +0200

    Add personal logo and link to portfolio website

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,33 @@ import React from "react"
 import PropTypes from "prop-types"
 
 import "./layout.css"
+import GatsbyImage from "gatsby-image";
+import {useStaticQuery, graphql} from "gatsby";
 
-const Layout = ({ children }) => (
-  <div className="page-layout">
-    <main>{children}</main>
-  </div>
-)
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query LayoutQuery {
+      image: file(relativePath: { eq: "logo.png" }) {
+        childImageSharp {
+          fixed(height: 70, width: 30) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <div className="page-layout">
+      <div className="logo-container">
+        <a href="https://pawelzdralewicz.pl/" rel="noopener noreferrer" target="_blank">
+          <GatsbyImage fixed={data.image.childImageSharp.fixed} />
+        </a>
+      </div>
+      <main>{children}</main>
+    </div>
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
